176a148789f728ec4ac0ad730dcf0d7b002a2d95
Devin W. Leaman <dleaman212@gmail.com>
2020-06-05 21:16:34 -0500

    Exported proper class from index file

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
+import { ReportsAPI } from './endpoints/Reports'
+import { TogglAPI } from './endpoints/Toggl'
+import { APIConfig } from './interfaces/APIConfig'
+
 export class TogglAid {
-  
+  togglAPI: TogglAPI
+  reportsAPI: ReportsAPI
+
+  constructor(configOpts: APIConfig) {
+    if (configOpts) {
+      this.togglAPI = new TogglAPI(configOpts)
+      this.reportsAPI = new ReportsAPI(configOpts)
+    } else {
+      throw new Error('You must provide the configOpts to use this library.')
+    }
+  }
 }
