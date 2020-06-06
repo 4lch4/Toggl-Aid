aa96e0786c852122e29bb0108ef3a96e8b41ebc3
Devin W. Leaman <dleaman212@gmail.com>
2020-06-05 20:10:24 -0500

    Start of the Clients endpoint

--- src/endpoints/Toggl/Clients.ts
+++ src/endpoints/Toggl/Clients.ts
@@ -1,5 +1,14 @@
+import { Client_Create, Client_POST_RES } from '../../interfaces/Client';
 import { BaseEndpoint } from '../BaseEndpoint';
 
 export class Clients extends BaseEndpoint {
-  
+  async createClient(client: Client_Create): Promise<Client_POST_RES> {
+    try {
+      const res = await this.axios.performRequest('POST', '/clients', {
+        data: client
+      })
+
+      return res
+    } catch (err) { return err }
+  }
 }
