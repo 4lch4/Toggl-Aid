16f9592f1884b4c1e6961df11424f33f424af70b
Devin W. Leaman <dleaman212@gmail.com>
2020-06-05 21:14:45 -0500

    Start of ReportsAPI endpoints

--- src/endpoints/Reports/index.ts
+++ src/endpoints/Reports/index.ts
@@ -1,3 +1,22 @@
-import { BaseEndpoint } from '../BaseEndpoint';
+import { APIConfig } from '../../interfaces/APIConfig'
+import { Details } from './Details'
+import { Summary } from './Summary'
+import { Weekly } from './Weekly'
 
-export class Reports extends BaseEndpoint {}
+export class ReportsAPI {
+  weekly: Weekly
+  details: Details
+  summary: Summary
+
+  constructor(configOpts: APIConfig) {
+    if (configOpts) {
+      this.weekly = new Weekly()
+      this.details = new Details()
+      this.summary = new Summary()
+    } else {
+      throw new Error(
+        'You must provide the configOpts parameter to use this library.'
+      )
+    }
+  }
+}
--- src/utils/Defaults.ts
+++ src/utils/Defaults.ts
@@ -1,10 +1,21 @@
+const APIUrls = {
+  TogglAPI: 'https://www.toggl.com/api/v8',
+  ReportsAPI: 'https://www.toggl.com/reports/api/v2'
+}
+
 /**
  * The default values to be used for interacting with the Toggl API. At the time
  * of creation, the two defaults stored are the baseUrl for the API, and the
  * headers to be sent along w/ each request.
  */
 const Defaults = {
-  baseUrl: 'https://www.toggl.com/api/v8',
+  reportsAPI: {
+    baseUrl: APIUrls.ReportsAPI,
+    weeklyReportUrl: `${APIUrls.ReportsAPI}/weekly`,
+    detailsReportUrl: `${APIUrls.ReportsAPI}/details`,
+    summaryReportUrl: `${APIUrls.ReportsAPI}/summary`
+  },
+  baseTogglUrl: APIUrls.TogglAPI,
   headers: { 'Content-Type': 'application/json' }
 }
 
