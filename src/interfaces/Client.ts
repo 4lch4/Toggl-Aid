e554b8054138628fa2e9804338f914078c72aa9e
Devin W. Leaman <dleaman212@gmail.com>
2020-06-05 20:10:45 -0500

    Added some more Client interfaces

--- src/interfaces/Client.ts
+++ src/interfaces/Client.ts
@@ -1,3 +1,4 @@
+/** The standard model for a Client object. */
 export interface Client {
   /** The name of the client (unique in workspace). */
   name: string
@@ -12,4 +13,22 @@ export interface Client {
    * Timestamp sent in the response, indicates the time client was last updated.
    */
   at: Date
+}
+
+/**
+ * The response from the Toggle API after successfully creating a new Client.
+ */
+export interface Client_POST_RES {
+  id: number;
+  wid: number;
+  name: string;
+  at: Date;
+}
+
+/** The object to be sent to the API when creating a new Client. */
+export interface Client_Create {
+  name: string
+  wid: number
+  notes?: string
+  timestamp: Date
 }
