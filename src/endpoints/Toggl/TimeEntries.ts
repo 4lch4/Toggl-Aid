b1514a631362789263720bd57a93a8e1094f2509
Devin W. Leaman <dleaman212@gmail.com>
2020-06-06 13:32:56 -0500

    Added some more endpoints to TimeEntries

--- src/endpoints/Toggl/TimeEntries.ts
+++ src/endpoints/Toggl/TimeEntries.ts
@@ -1,8 +1,13 @@
-import { TimeEntry_POST, TimeEntry_POST_RES } from '../../interfaces/TimeEntry'
+import {
+  TimeEntry_NEW,
+  TimeEntry_NEW_RES,
+  TimeEntry_START,
+  TimeEntry_START_RES
+} from '../../interfaces/TimeEntry'
 import { BaseEndpoint } from '../BaseEndpoint'
 
 export class TimeEntries extends BaseEndpoint {
-  async createTimeEntry(timeEntry: TimeEntry_POST): Promise<TimeEntry_POST_RES> {
+  async createTimeEntry(timeEntry: TimeEntry_NEW): Promise<TimeEntry_NEW_RES> {
     try {
       const res = await this.axios.performRequest('POST', '/time_entries', {
         data: { time_entry: timeEntry }
@@ -13,4 +18,46 @@ export class TimeEntries extends BaseEndpoint {
       return err
     }
   }
+
+  async startTimeEntry(
+    timeEntry: TimeEntry_START
+  ): Promise<TimeEntry_START_RES> {
+    try {
+      const res = await this.axios.performRequest(
+        'POST',
+        '/time_entries/start',
+        {
+          data: timeEntry
+        }
+      )
+
+      return res.data ? res.data : res.status
+    } catch (err) {
+      return err
+    }
+  }
+
+  async getRunningTimeEntry(): Promise<TimeEntry_GET_RES> {
+    try {
+      const res = await this.axios.performRequest(
+        'GET',
+        '/time_entries/current'
+      )
+
+      return res.data ? res.data : res.status
+    } catch (err) {
+      return err
+    }
+  }
+}
+
+export interface TimeEntry_GET_RES {
+  id: number
+  wid: number
+  pid: number
+  billable: boolean
+  start: string
+  duration: number
+  description: string
+  at: string
 }
--- src/interfaces/TimeEntry.ts
+++ src/interfaces/TimeEntry.ts
@@ -1,4 +1,4 @@
-export interface TimeEntry_POST {
+export interface TimeEntry_NEW {
   description: string
   tags: string[]
   duration: number
@@ -7,7 +7,25 @@ export interface TimeEntry_POST {
   created_with: string
 }
 
-export interface TimeEntry_POST_RES {
+export interface TimeEntry_NEW_RES {
+	id: number;
+	pid: number;
+	wid: number;
+	billable: boolean;
+	start: string;
+	duration: number;
+	description: string;
+	tags: string[];
+}
+
+export interface TimeEntry_START {
+	description: string;
+	tags: string[];
+	pid: number;
+	created_with: string;
+}
+
+export interface TimeEntry_START_RES {
 	id: number;
 	pid: number;
 	wid: number;
