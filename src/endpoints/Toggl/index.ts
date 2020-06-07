91447f9cd0e8b581fb3512b3c982cfb6ab8884df
Devin W. Leaman <dleaman212@gmail.com>
2020-06-07 15:34:42 -0500

    Forgot to export the Workspace endpoints

--- src/endpoints/Toggl/index.ts
+++ src/endpoints/Toggl/index.ts
@@ -1,13 +1,16 @@
 import { APIConfig } from '../../interfaces/APIConfig'
 import { Clients } from './Clients'
 import { TimeEntries } from './TimeEntries'
+import { Workspaces } from './Workspaces'
 
 export class TogglAPI {
   clients: Clients
   timeEntries: TimeEntries
+  workspaces: Workspaces
 
   constructor(configOpts: APIConfig) {
     this.clients = new Clients(configOpts)
     this.timeEntries = new TimeEntries(configOpts)
+    this.workspaces = new Workspaces(configOpts)
   }
 }
